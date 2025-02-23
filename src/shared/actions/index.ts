c2246bd9d1eab403e6fa9a76e9d3604b7e897f98
Айяр Халилов <super.ayar-04@yandex.ru>
2025-02-23 22:47:52 +0300

    done

--- src/shared/actions/index.ts
+++ src/shared/actions/index.ts
@@ -16,7 +16,7 @@ export const useUpgradePerkAction = (type: 'health' | 'shield' | 'damage') => {
         onSettled: () => queryClient.invalidateQueries(),
         onError: (error) => {
             if (error instanceof AxiosError) {
-                if (error.response?.status === 400) {
+                if (error.response?.data.error === 'Not enough boxi') {
                     messageModal.open({
                         title: 'Not enough $BOXI',
                         message: "You don't have required amount of $BOXI to buy this upgrade.",
