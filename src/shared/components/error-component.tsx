7923cf95d5457da6f9365a320d31b77f6ef6b333
Айяр Халилов <super.ayar-04@yandex.ru>
2025-02-23 16:55:36 +0300

    add commit hash

--- src/shared/components/error-component.tsx
+++ src/shared/components/error-component.tsx
@@ -12,8 +12,9 @@ const ErrorComponent: ErrorRouteComponent = (props) => {
         <div className='flex h-screen max-h-full flex-col items-center justify-center p-16'>
             <div className='mb-2 text-center leading-tight'>Something went wrong. Contact with administrator.</div>
             <div className='text-center text-sm opacity-50'>Trace ID: {props.error.message}</div>
+            <div className='text-center text-sm opacity-50'>Build: {__COMMIT_HASH__}</div>
 
-            <button type='button' onClick={sendTraceId} className='mt-8 rounded-lg bg-white px-4 py-2 font-semibold text-black'>
+            <button type='button' onClick={sendTraceId} className='mt-4 rounded-lg bg-white px-4 py-2 font-semibold text-black'>
                 Contact
             </button>
         </div>
