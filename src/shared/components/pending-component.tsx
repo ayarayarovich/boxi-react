18e4672ff8f6dd18cb0521eac7b6741f01d33dca
Айяр Халилов <super.ayar-04@yandex.ru>
2025-02-23 15:53:33 +0300

    add messages

--- src/shared/components/pending-component.tsx
+++ src/shared/components/pending-component.tsx
@@ -1,7 +1,45 @@
+import { useMemo } from 'react'
+import { useStopwatch } from 'react-timer-hook'
+
+import { AnimatePresence, motion, Variants } from 'motion/react'
+
 export default function PendingComponent() {
+    const sw = useStopwatch({ autoStart: true })
+    const message = useMemo(() => {
+        if (sw.totalSeconds >= 30) {
+            return 'for some reason takes too long...'
+        }
+        if (sw.totalSeconds >= 10) {
+            return 'something looks off...'
+        }
+        if (sw.totalSeconds >= 5) {
+            return 'just a little bit...'
+        }
+        if (sw.totalSeconds >= 1) {
+            return 'will be ready soon...'
+        }
+        return ''
+    }, [sw.totalSeconds])
+    const variants = useMemo(
+        () =>
+            ({
+                hidden: {
+                    opacity: 0,
+                },
+                visible: {
+                    opacity: 0.75,
+                },
+            }) satisfies Variants,
+        [],
+    )
     return (
         <div className='flex h-screen max-h-full flex-col items-center justify-center p-16'>
-            <div className='size-8 animate-spin rounded-full border-8 border-t-0 border-b-0'></div>
+            <div className='mb-3 size-8 animate-spin rounded-full border-4 border-y-transparent'></div>
+            <AnimatePresence mode='wait'>
+                <motion.div variants={variants} className='text-sm' initial='hidden' animate='visible' exit='hidden' key={message}>
+                    {message ? message : <>&nbsp;</>}
+                </motion.div>
+            </AnimatePresence>
         </div>
     )
 }
