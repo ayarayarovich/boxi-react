a286d8196beb96c4b8e85af41168542d7f62735c
Айяр Халилов <super.ayar-04@yandex.ru>
2025-02-23 21:55:26 +0300

    wip

---
+++ src/shared/modals/message-modal.tsx
@@ -0,0 +1,97 @@
+import { Dialog, Modal, ModalOverlay } from 'react-aria-components'
+
+import { AnimatePresence, motion } from 'motion/react'
+
+import { useModalInstance } from '@/lib/ayarayarovich-modals'
+import { CrossIcon } from '@/lib/icons'
+
+const MotionModal = motion.create(Modal)
+const MotionModalOverlay = motion.create(ModalOverlay)
+
+export interface Data {
+    title: string
+    message: string
+}
+
+export default function MessageModalComponent() {
+    const { isOpen, close, data } = useModalInstance<Data>()
+
+    return (
+        <>
+            <AnimatePresence>
+                {isOpen && (
+                    <MotionModalOverlay
+                        onOpenChange={(v) => !v && close()}
+                        initial={{
+                            opacity: 0,
+                            backdropFilter: 'blur(0px)',
+                        }}
+                        animate={{
+                            opacity: 1,
+                            backdropFilter: 'blur(5px)',
+                        }}
+                        exit={{
+                            opacity: 0,
+                            backdropFilter: 'blur(0px)',
+                            transition: {
+                                duration: 0.5,
+                                ease: 'easeInOut',
+                            },
+                        }}
+                        isOpen={isOpen}
+                        isDismissable
+                        className='bg-dark/40 fixed inset-x-0 top-0 z-50 h-dvh overflow-hidden'
+                    >
+                        <div className='h-dvh w-full overflow-y-auto'>
+                            <div className='flex min-h-full items-center justify-center p-4'>
+                                <MotionModal
+                                    initial={{
+                                        opacity: 0,
+                                        scale: 0.9,
+                                    }}
+                                    animate={{
+                                        y: 0,
+                                        opacity: 1,
+                                        scale: 1,
+                                    }}
+                                    exit={{
+                                        opacity: 0,
+                                        scale: 1.1,
+                                        transition: {
+                                            duration: 0.25,
+                                            ease: 'easeInOut',
+                                        },
+                                    }}
+                                    className='w-full max-lg:flex-1 lg:max-w-lg'
+                                >
+                                    <Dialog className='bg-dark-elevated flex w-full flex-col items-stretch rounded-xl p-6 ring-[#292929] ring-offset-0 transition-shadow outline-none focus:ring-2'>
+                                        {({ close }) => (
+                                            <>
+                                                <div className='relative mb-8'>
+                                                    <h2 className='text-center text-xl font-semibold'>{data.title}</h2>
+                                                    <button className='absolute -top-4 -right-4 p-4' type='button' onClick={close}>
+                                                        <CrossIcon className='h-4' />
+                                                    </button>
+                                                </div>
+
+                                                <div className='mb-2 text-center leading-tight'>{data.message}</div>
+
+                                                <button
+                                                    type='button'
+                                                    onClick={close}
+                                                    className='mt-4 rounded-lg bg-white px-4 py-2 font-semibold text-black'
+                                                >
+                                                    Ok
+                                                </button>
+                                            </>
+                                        )}
+                                    </Dialog>
+                                </MotionModal>
+                            </div>
+                        </div>
+                    </MotionModalOverlay>
+                )}
+            </AnimatePresence>
+        </>
+    )
+}
